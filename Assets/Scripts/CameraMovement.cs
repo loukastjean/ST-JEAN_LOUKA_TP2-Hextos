9e721fcda559bb9d0dc22fb3ba0193bf1c26072a
loukastjean <2357611@cegepat.qc.ca>
2025-02-27 20:18:44 -0500

    Fix max et min de camera

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -14,6 +14,10 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(Input.GetAxis("Horizontal") * cameraSpeed, Input.GetAxis("Vertical") * cameraSpeed, 0);
+        if ((Input.GetAxis("Horizontal") < 0 && transform.position.x > -15) || (Input.GetAxis("Horizontal") > 0 && transform.position.x < 17))
+            transform.position += new Vector3(Input.GetAxis("Horizontal") * cameraSpeed, 0, 0);
+        
+        if ((Input.GetAxis("Vertical") < 0 && transform.position.y > -7) || (Input.GetAxis("Vertical") > 0 && transform.position.y < 7))
+            transform.position += new Vector3(0, Input.GetAxis("Vertical") * cameraSpeed, 0);
     }
 }
